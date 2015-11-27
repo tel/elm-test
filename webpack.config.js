4282bec05f6027f528f51662b0598dbe11050754
Joseph Abrahamson <me@jspha.com>
2015-11-27 18:43:16 -0500

    working webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@
 
 var path = require("path");
 var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: "./app/index.js",
@@ -9,16 +10,26 @@ module.exports = {
         path: path.join(__dirname, "public"),
         filename: "app.js"
     },
+    devtool: "source-map",
     module: {
         loaders: [
-            { test: /\.elm$/, loaders: ["exports?Elm", "elm"] },
-            { test: /\.scss$/, loaders: ["style-loader", "css", "sass"] }
+            {
+                test: /\.elm$/,
+                loaders: ["exports?Elm", "elm"]
+            },
+            {
+                test: /\.scss$/,
+                loader:
+                    ExtractTextPlugin.extract(["css?sourceMap", "sass?sourceMap"])
+                    // ["style?sourceMap", "css?sourceMap", "sass?sourceMap"]
+            }
         ]
     },
     elmLoader: {
         dir: "./elm"
     },
     plugins: [
+        new ExtractTextPlugin("styles.css"),
         new HtmlWebpackPlugin({
             title: "StudyTeam",
             hash: true,
@@ -27,6 +38,7 @@ module.exports = {
         })
     ],
     devServer: {
+        contentBase: "./public",
         historyApiFallback: true
     }
 };
